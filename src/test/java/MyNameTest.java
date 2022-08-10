7427fbe95e88c047eec9d9eef6f958d251852a39
e.a.zinovev <egor.zinovjev2013@yandex.ru>
2022-08-10 11:38:01 +0500

    NegativeAge, EmptyName, NullName validation added

--- src/test/java/MyNameTest.java
+++ src/test/java/MyNameTest.java
@@ -12,4 +12,19 @@ public class MyNameTest extends ProgramTest {
     public void testMyNameValidationZeroAge() {
         Assert.assertEquals(program.sayMyName("Yegor Zinovyev", 0), "Name is Yegor Zinovyev (0)");
     }
+
+    @Test
+    public void testMyNameValidationNegativeAge() {
+        Assert.assertThrows(IllegalStateException.class, () -> program.sayMyName("Yegor Zinovyev", -1));
+    }
+
+    @Test
+    public void testMyNameValidationEmptyName() {
+        Assert.assertThrows(IllegalStateException.class, () -> program.sayMyName("", 1));
+    }
+
+    @Test
+    public void testMyNameValidationNullName() {
+        Assert.assertThrows(IllegalStateException.class, () -> program.sayMyName(null, 1));
+    }
 }
